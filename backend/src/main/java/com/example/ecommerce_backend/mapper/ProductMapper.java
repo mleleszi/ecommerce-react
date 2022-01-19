c0a6fc680b91d869322fb5076cf8254ecb65f03a
mleleszi <mleleszi@icloud.com>
2022-01-19 11:12:03 +0100

    get all products mapping

--- backend/src/main/java/com/example/ecommerce_backend/mapper/ProductMapper.java
+++ backend/src/main/java/com/example/ecommerce_backend/mapper/ProductMapper.java
@@ -2,12 +2,14 @@ package com.example.ecommerce_backend.mapper;
 
 import com.example.ecommerce_backend.models.Product;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.mybatis.spring.annotation.MapperScan;
 
 import java.util.List;
 
 @Mapper
+@MapperScan
 public interface ProductMapper{
-
     List<Product> getAllProducts();
 }
--- backend/src/test/java/com/example/ecommerce_backend/EcommerceBackendApplicationTests.java
+++ backend/src/test/java/com/example/ecommerce_backend/EcommerceBackendApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.ecommerce_backend;
 
+import com.example.ecommerce_backend.mapper.ProductMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EcommerceBackendApplicationTests {
 
+    @Autowired
+    ProductMapper productMapper;
+
     @Test
     void contextLoads() {
+
     }
+
+    @Test
+    public void test(){
+        productMapper.getAllProducts().forEach(System.out::println);
+    }
+
+
+
+
 
 }
